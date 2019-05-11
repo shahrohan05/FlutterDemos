c02950d59026d97b2fd8690c21e90d9d5333a6af
shahrohan05 <shahrohan07@live.com>
2019-05-11 14:45:32 +0530

    scaffold, appbar, local asset image & app icon

--- demo_1/lib/main.dart
+++ demo_1/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo 1'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[800],
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(0),
+            child: Image(image: AssetImage('images/diamond.png')),
+          ),
+        ),
       ),
     ),
   );
