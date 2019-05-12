2c273234bf6b3f612a32c7158f39ed55b826a63a
shahrohan05 <shahrohan07@live.com>
2019-05-12 17:02:54 +0530

    added dice images and created dice view

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -8,6 +8,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  'Dice',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(16.0),
+                        child: Image.asset('images/dice1.png'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(16.0),
+                        child: Image.asset('images/dice2.png'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
