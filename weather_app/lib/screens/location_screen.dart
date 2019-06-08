6fc2f4e58c6711368b0a6dcfbdc27a5a1a070bc8
shahrohan05 <shahrohan07@live.com>
2019-06-08 22:33:55 +0530

    weather app project, initial commit

--- weather_app/lib/screens/location_screen.dart
+++ weather_app/lib/screens/location_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LocationScreen extends StatelessWidget {
+  void getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print('User\'s Position: $position');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text('GET LOCATION'),
+            onPressed: () {
+              getLocation();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
