bd02496e5d1b8d7b0d1da90dffd17d091c7ce18a
shahrohan05 <shahrohan07@live.com>
2019-06-17 11:14:01 +0530

    adding a new bitcoin price project

---
+++ bitcoin_prices/README.md
@@ -0,0 +1,3 @@
+# bitcoin_prices
+
+A sample flutter project to show bitcoin prices in different currencies.
---
+++ bitcoin_prices/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bitcoin Prices',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Bitcoin Prices'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Test Bitcoin Prices',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
